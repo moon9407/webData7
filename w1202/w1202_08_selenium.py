a70686f58ef96d62b6c4b7f628c268e10ec7f09e
moon9407 <xzc7001@naver.com>
2025-12-05 09:50:24 +0900

    webData

--- w1202/w1202_08_selenium.py
+++ w1202/w1202_08_selenium.py
@@ -3,17 +3,14 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
-# requests라이브러리, selenium라이브러리 차이점. 속도차이
-
-# 크롬드라이버를 활용해서 크롬 브라우저를 제어할 수 있음
+# requests라이브러리, selenium라이브러리 차이점.
+# 크롬드라이버를 활용해서 크롬브라우저를 제어할수 있음.
 browser = webdriver.Chrome()
-# 크롬 브라우저 창이 열림
+# 크롬브라우저 창이 열림.
 browser.get("https://comic.naver.com/index")
 
-time.sleep(8)   # 3초동안 대기
-# input("enter키 입력 시 다시실행") # 창 실행  후 터미널 창에 엔터키 입력시 데이터 출력
+time.sleep(7) # 3초 대기후 실행
 soup = BeautifulSoup(browser.page_source,"lxml")
-
 # 파일저장
 with open("webtoon_browser.html","w",encoding="utf8") as f:
     f.write(soup.prettify())
@@ -22,11 +19,8 @@ with open("webtoon_browser.html","w",encoding="utf8") as f:
 
 
 
-
-
-
-# url = "https://www.daum.net/"
-# headers = {"User-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"}
+# url="https://www.daum.net/"
+# headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"}
 # res = requests.get(url,headers=headers)
 # res.raise_for_status()
 # soup = BeautifulSoup(res.text,"lxml")
